3601c19c9bf224d4bb24bb95c4daed6eecd384df
etper <piotr.brzezinski9@gmail.com>
2026-07-13 20:12:52 +0200

    fixes fixes fixeeeeeees

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -1,8 +1,19 @@
+@tool
 extends Area2D
+
+@export var sprite: Texture2D:
+	set(value):
+		sprite = value
+		if has_node("Sprite2D"):
+			$Sprite2D.texture = value
 
 @export var events: Array[EventCommand]
 
 var busy := false
+
+func _ready():
+	if sprite:
+		$Sprite2D.texture = sprite
 
 func interact():
 	if busy:
