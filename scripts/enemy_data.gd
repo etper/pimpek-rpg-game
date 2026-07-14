3dc318a20e9b181bea49af8d69abaac80e91b669
etper <piotr.brzezinski9@gmail.com>
2026-07-14 13:23:04 +0200

    revert

--- scripts/enemy_data.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name EnemyData
-
-@export var name := ""
-@export var sprite: Texture2D
-
-@export var max_hp := 10
-@export var attack := 3
-@export var defense := 1
--- scripts/player_data.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name PlayerData
-
-@export var name := "Pimpek"
-
-@export var max_hp := 30
-@export var attack := 5
-@export var defense := 2
