01836757f929985ea055c4ffcaab63ef38a7ce11
etper <piotr.brzezinski9@gmail.com>
2026-07-14 13:56:00 +0200

    battle milestone 2

--- scenes/battle.gd
+++ scenes/battle.gd
@@ -1,4 +1,43 @@
 extends Node2D
 
-func _on_button_pressed() -> void:
-	BattleManager.end_battle()
+@onready var menu_labels = [
+	$Menu/Attack,
+	$Menu/Item,
+	$Menu/Run,
+]
+
+var selected := 0
+
+func _ready():
+	update_menu()
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("move_down"):
+		selected = (selected + 1) % menu_labels.size()
+		update_menu()
+
+	elif Input.is_action_just_pressed("move_up"):
+		selected = (selected - 1 + menu_labels.size()) % menu_labels.size()
+		update_menu()
+
+	elif Input.is_action_just_pressed("interact"):
+		choose_option()
+
+func update_menu():
+	var names = [
+		"Attack",
+		"Item",
+        "Run"
+	]
+
+	for i in menu_labels.size():
+		menu_labels[i].text = ("> " if i == selected else "  ") + names[i]
+
+func choose_option():
+	match selected:
+		0:
+			print("Attack")
+		1:
+			print("Item")
+		2:
+			print("Run")
