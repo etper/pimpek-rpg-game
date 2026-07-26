3113fc52b698c136cba27e2e71baf4efb3c7ac22
etper <piotr.brzezinski9@gmail.com>
2026-07-26 14:44:14 +0200

    intro fade in complete

--- main.gd
+++ main.gd
@@ -2,20 +2,22 @@ extends Node2D
 
 const ROOM_MUSIC = preload("res://music/Pimpek Room.mp3")
 
-@onready var fade: ColorRect = $Fade
+@onready var fade: ColorRect = $UI/Fade
 
 func _ready():
-	# Start completely black
+	# Make sure black overlay is visible first.
+	fade.show()
 	fade.color = Color(0, 0, 0, 1)
 
-	# Start the area's music
 	MusicManager.play(ROOM_MUSIC)
 
-	# Fade the game into view
-	var tween = create_tween()
-	tween.tween_property(fade, "color:a", 0.0, 4.0)
+	# Allow one fully black frame to render.
+	await get_tree().process_frame
 
-	# Fade music in at the same time
+	var tween = create_tween()
+	tween.set_parallel(true)
+
+	tween.tween_property(fade, "color:a", 0.0, 4.0)
 	MusicManager.fade_in(4.0)
 
 	await tween.finished
--- scenes/cutscene.gd
+++ scenes/cutscene.gd
@@ -2,11 +2,21 @@ extends Control
 
 @onready var video: VideoStreamPlayer = $Video
 
+var leaving := false
+
 func _ready():
 	MusicManager.stop()
-	video.finished.connect(_on_video_finished)
+	video.finished.connect(_finish_cutscene)
 	video.play()
 
+func _unhandled_input(event):
+	if event.is_action_pressed("cancel"):
+		_finish_cutscene()
 
-func _on_video_finished():
+func _finish_cutscene():
+	if leaving:
+		return
+
+	leaving = true
+	video.stop()
 	get_tree().change_scene_to_file("res://main.tscn")
